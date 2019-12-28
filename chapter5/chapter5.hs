199f3fc13182661500d068f93510dc693a8b1dc2
Andy Wang <climber2002@gmail.com>
2019-12-28 22:24:07 +1100

    ch5

---
+++ chapter5/chapter5.hs
@@ -0,0 +1,23 @@
+data TimeMachine = TM { manufacturer :: String, year :: Integer } deriving (Eq, Show)
+
+timeMachinesFrom :: String -> Integer -> [TimeMachine]
+timeMachinesFrom mf y = TM mf y : timeMachinesFrom mf (y + 1)
+
+timelyIncMachines :: [TimeMachine]
+timelyIncMachines = timeMachinesFrom "Timely Inc." 100
+
+allNumbers :: [Integer]
+allNumbers = allNumbersFrom 1
+
+allNumbersFrom :: Integer -> [Integer]
+allNumbersFrom n = n : allNumbersFrom (n + 1)
+
+fibonacci :: [Integer]
+fibonacci = 0 : 1 : zipWith (+) fibonacci (tail fibonacci)
+
+specialOffer :: [TimeMachine]
+specialOffer = cycle [TM m 2005, TM m 1994, TM m 908]
+                where m = "Timely Inc."
+
+fibonacci2 :: [Integer]
+fibonacci2 = map fst $ iterate (\(n, n1) -> (n1, n + n1)) (0, 1)               
