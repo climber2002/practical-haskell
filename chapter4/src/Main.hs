28c6d50f3082ef15c446562783d291951c3c8998
Andy Wang <climber2002@gmail.com>
2020-01-01 09:43:43 +1100

    ch4

--- chapter4/src/Main.hs
+++ chapter4/src/Main.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import qualified Data.Map as M
+import Data.Tree
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
+
+preOrder :: (a -> b) -> Tree a -> [b]
+preOrder f (Node v subTrees)
+  = let subtreesTraversed = concat $ map (preOrder f) subTrees
+    in f v : subtreesTraversed
