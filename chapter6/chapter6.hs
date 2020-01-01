2f58c667b8f0e637a62efc4b4f90dcaf4ea46f09
Andy Wang <climber2002@gmail.com>
2020-01-01 21:15:25 +1100

    Add kMeans

--- chapter6/chapter6.hs
+++ chapter6/chapter6.hs
@@ -4,22 +4,21 @@
 import Data.List
 import qualified Data.Map as M
 
-class Vector v where
+class Ord v => Vector v where
   distance :: v -> v -> Double
+  centroid :: [v] -> v
 
 instance Vector (Double, Double) where
   distance (a, b) (c, d) = sqrt $ (c - a) * (c - a) + (b - d) * (b - d)
+  centroid lst = let (u, v) = foldr (\(a, b) (c, d) -> (a + c, b + d)) (0, 0) lst
+                     n = fromIntegral $ length lst
+                     in (u / n, v / n)
 
 class Vector v => Vectorizable e v where
   toVector :: e -> v
 
 instance Vectorizable (Double, Double) (Double, Double) where
   toVector = id
-
--- kMeans :: (Vector v, Vectorizable e v)
---        => (Int -> [e] -> [v]) -- number of centroids
---        -> [e] -- the information
---        -> [v] -- centroids after convergence
 
 clusterAssignmentPhase :: (Ord v, Vector v, Vectorizable e v)
                        => [v] -> [e] -> M.Map v [e]
@@ -32,5 +31,32 @@ clusterAssignmentPhase centroids points =
                                           (distance y $ toVector p)
 
 
+newCentroidPhase :: (Vector v, Vectorizable e v) => M.Map v [e] -> [(v, v)]
+newCentroidPhase = M.toList . fmap (centroid . map toVector)
 
+shouldStop :: (Vector v) => [(v, v)] -> Double -> Bool
+shouldStop centroids threshold =
+  foldr (\(x, y) s -> s + distance x y) 0.0 centroids < threshold
 
+kMeans :: (Vector v, Vectorizable e v)
+       => (Int -> [e] -> [v])   -- initialization function
+       -> Int                   -- number of centroids
+       -> [e]                   -- the information
+       -> Double                -- threshold
+       -> [v]                   -- centroids after convergence
+kMeans i k points = kMeans' (i k points) points
+
+kMeans' :: (Vector v, Vectorizable e v)
+        => [v] -> [e] -> Double -> [v]
+kMeans' centroids points threshold =
+  let assignments = clusterAssignmentPhase centroids points
+      oldNewCentroids = newCentroidPhase assignments
+      newCentroids = map snd oldNewCentroids
+   in if shouldStop oldNewCentroids threshold
+      then newCentroids
+      else kMeans' newCentroids points threshold
+
+initializeSimple :: Int -> [e] -> [(Double, Double)]
+initializeSimple 0 _ = []
+initializeSimple n v = (fromIntegral n, fromIntegral n)
+                     : initializeSimple (n - 1) v
